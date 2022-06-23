9d874fce3fba78d4a9a9b24aaeaa115699fccf4d
ArteiusWasTaken <sebastiancifer@gmail.com>
2022-06-22 19:04:17 -0500

    sss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import type { NextPage } from "next";
 import { BaseLayout } from "../components/layout";
-import Login from "../components/ui/Login";
-
+import React, { useCallback, useEffect } from "react";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+import { auth, firebase } from "../firebase";
 const Home: NextPage = () => {
+  var uiConfig = {
+    signInFlow: "popup",
+    signInSuccessUrl: "/dashboard",
+    tosUrl: "/terms-of-service",
+    privacyPolicyUrl: "/privacy-policy",
+    signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+  };
+
+  const loadFirebaseui = useCallback(async () => {
+    const firebaseui = await import("firebaseui");
+    const firebaseUi =
+      firebaseui.auth.AuthUI.getInstance() ||
+      new firebaseui.auth.AuthUI(firebase.auth());
+    firebaseUi.start("#firebaseui-auth-container", uiConfig);
+  }, [firebase, uiConfig]);
+
+  useEffect(() => {
+    loadFirebaseui();
+  }, []);
   return (
     <BaseLayout>
-      <Login />
+      <div id="firebaseui-auth-container"></div>;
     </BaseLayout>
   );
 };
