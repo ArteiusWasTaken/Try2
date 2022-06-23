ae5673744f1b4b2d34b5d548012f591a11a42ed3
ArteiusWasTaken <sebastiancifer@gmail.com>
2022-06-23 11:26:39 -0500

    vcx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
 import type { NextPage } from "next";
 import { BaseLayout } from "../components/layout";
 import React, { useCallback, useEffect } from "react";
-import * as firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
-import { auth, firebase } from "../firebase";
+import { firebase } from "../firebase";
 const Home: NextPage = () => {
   var uiConfig = {
     signInFlow: "popup",
     signInSuccessUrl: "/dashboard",
     tosUrl: "/terms-of-service",
     privacyPolicyUrl: "/privacy-policy",
-    signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    ],
   };
 
   const loadFirebaseui = useCallback(async () => {
-    const firebaseui = await import("firebaseui");
+    const firebaseui = await import("./npm__es");
     const firebaseUi =
       firebaseui.auth.AuthUI.getInstance() ||
       new firebaseui.auth.AuthUI(firebase.auth());
