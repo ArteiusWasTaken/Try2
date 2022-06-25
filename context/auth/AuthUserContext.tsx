de6531d6bc602274b47c836ab302a82c3b6dfd4e
ArteiusWasTaken <sebastiancifer@gmail.com>
2022-06-25 09:14:11 -0500

    cv

--- context/auth/AuthUserContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-interface ContextProps {
-  authuser: any;
-  loading: boolean;
-  auth: any;
-}
-
-export const AuthUserContext = createContext({} as ContextProps);
